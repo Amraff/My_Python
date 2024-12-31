48a5e749632e23c42704543dc7292f94c80dc9c9
Raphew <raphewkarim@gmail.com>
2024-12-31 18:47:10 +0000

    My first python commit

---
+++ test.py
@@ -0,0 +1,10 @@
+name = "Raphew Karim Issah"
+age = 33
+height = 6.4
+is_student = True
+city = "Accra"
+print("My name is :", name)
+print("I am", age)
+print("My height is:", height)
+print("Is_student:", is_student)
+print("I live in:", city)
