444210697f4a98a4d1a71ff032921ca7b04a9b34
Raphew <raphewkarim@gmail.com>
2024-12-31 20:23:58 +0000

    Python day 2

--- test.py
+++ test.py
@@ -1,10 +1,24 @@
-num1 = 50
-num2 = 5
-result = num1 + num2
-print("the answer is:", result)
-result = num1 - num2
-print("The subtraction is:", result)
-result = num1 / num2
-print("the division is:", result)
-result = num1 % num2
-print("the modulus is:", result)
+#Checking the length of a value in a variable
+name = "Raphew Karim Issah"
+print("My name is:", name)
+length = len(name)
+print("Length of the string is:", length)
+
+#Printing my age and adding 1 to next year's age
+age = 33
+print("My age this year is:", age)
+next_year_age = age + 1
+print("My age next year is:", next_year_age)
+
+#joining independent variables together by concatenating
+first_name = "Raphew"
+mid_name = "Karim"
+last_name = "Issah"
+full_name = first_name + " " + mid_name + " " + last_name
+print("My full name is:", full_name)
+
+#simple age eligibility
+age = 17
+is_eligible_to_vote = age >= 18
+print("Eligible to vote:", is_eligible_to_vote)
+
